94321e8f66cec8e3c3ee8a995a8dfdd926d52ce5
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2021-04-01 22:38:55 +0300

    Update buisness.cpp

--- buisness.cpp
+++ buisness.cpp
@@ -1,29 +1,6 @@
 #include "buisness.h"
 using namespace std;
 
-void calc_metric(vector<double> arr, int col_metric, double* min, double* max, double* avg){
-    sort(arr.begin(), arr.end());
-    *min = 0;
-    *max = 0;
-    *avg = 0;
-    if (arr.size() != 0){
-        *min = arr[0];
-        *max = arr[arr.size() - 1];
-        if (arr.size() % 2 == 0){
-            *avg = (arr[arr.size() / 2] + arr[arr.size() / 2 - 1]) / 2.0;
-        } else {
-            *avg = arr[arr.size() / 2];
-        }
-    }
-}
-
-bool is_csv_file(string path){
-    int index = path.find(".csv");
-    if (index == -1)
-        return false;
-    return true;
-}
-
 bool is_digit(char c){
     if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0')
         return true;
