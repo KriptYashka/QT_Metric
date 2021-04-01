3ff68977c05ce7a325538216f4da4526d84fd39d
KriptYashka <58609689+KriptYashka@users.noreply.github.com>
2021-04-02 01:09:50 +0300

    Возврат удаленной функции

--- buisness.cpp
+++ buisness.cpp
@@ -19,6 +19,29 @@ bool is_normal_metric(string text){
     return true;
 }
 
+void calc_metric(vector<double> arr, int col_metric, double* min, double* max, double* avg){
+    sort(arr.begin(), arr.end());
+    *min = 0;
+    *max = 0;
+    *avg = 0;
+    if (arr.size() != 0){
+        *min = arr[0];
+        *max = arr[arr.size() - 1];
+        if (arr.size() % 2 == 0){
+            *avg = (arr[arr.size() / 2] + arr[arr.size() / 2 - 1]) / 2.0;
+        } else {
+            *avg = arr[arr.size() / 2];
+        }
+    }
+}
+
+bool is_csv_file(string path){
+int index = path.find(".csv");
+if (index == -1)
+return false;
+return true;
+}
+
 vector<string> split_line(string line){
     vector<string> result;
     string word = "";
